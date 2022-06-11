60d4135d967c777c49228736f1eff8dfce102c00
AndrewVasin <ironfeyd@gmail.com>
2022-06-11 18:54:55 +0300

    начальный коммит

---
+++ src/test/java/ru/vasin/tiny_url/TinyUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasin.tiny_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
